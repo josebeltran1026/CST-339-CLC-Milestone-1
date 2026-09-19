03299a6fc4657f057ce675586fbc4cd6f22ec88a
Jose Beltran <jose@example.com>
2026-09-19 14:07:34 -0700

    Add roster name REST endpoint

---
+++ src/main/java/com/example/cst339_clc_milestone1/controller/RosterNameController.java
@@ -0,0 +1,21 @@
+package com.example.cst339_clc_milestone1.controller;
+
+import com.example.cst339_clc_milestone1.dto.RosterNameDto;
+import com.example.cst339_clc_milestone1.service.RosterNameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RosterNameController {
+
+    private final RosterNameService rosterNameService;
+
+    public RosterNameController(RosterNameService rosterNameService) {
+        this.rosterNameService = rosterNameService;
+    }
+
+    @GetMapping("/roster-name")
+    public RosterNameDto getRosterName() {
+        return rosterNameService.getRosterName();
+    }
+}
